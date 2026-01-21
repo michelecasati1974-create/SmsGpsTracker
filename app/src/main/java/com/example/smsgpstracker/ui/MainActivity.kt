43b59d453101019a34039a4fe95b60fa23dbaaf8
Michele <michelecasati1974@alice.it>
2026-01-21 09:31:53 +0100

    Initial stable project - build ok

--- app/src/main/java/com/example/smsgpstracker/ui/MainActivity.kt
+++ app/src/main/java/com/example/smsgpstracker/ui/MainActivity.kt
@@ -1,13 +1,35 @@
 package com.example.smsgpstracker.ui
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.example.smsgpstracker.R
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    MaterialTheme {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "SmsGpsTracker avviata ✅")
+        }
     }
 }
